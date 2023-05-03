e8c2a6da2a685f6e3ca570768e4b810843e1acfc
MrKOT92 <mrkot9292@gmail.com>
2023-05-03 18:20:39 +0200

    soda hm

--- homework/hm33/src/Main.java
+++ homework/hm33/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Soda viva = new Soda("viva", "white");
+    System.out.println(viva);
+    System.out.println(viva.getColor());
+    viva.setColor("orange");
+    System.out.println(viva.getColor());
+
   }
 }
+
---
+++ homework/hm33/src/Soda.java
@@ -0,0 +1,17 @@
+public class Soda {
+  private String vitanta;
+  private String color;
+  public Soda(String vitanta, String color) {
+    this.vitanta = vitanta;
+    this.color = color;
+  }
+  public String getVitanta() {
+    return vitanta;
+  }
+  public String getColor() {
+    return color;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+}

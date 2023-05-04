0d026eba16d4e172ffd48f5ff6ba6c507f4702b8
MrKOT92 <mrkot9292@gmail.com>
2023-05-04 10:12:28 +0200

    complet task 1 2 3

--- lesson_34/src/Student.java
+++ lesson_34/src/Student.java
@@ -1,21 +1,31 @@
 public class Student {
- private  static int counter =0;
- private  final int id;
- private String name;
- public Student(String name){
-   id = ++counter;
-   this.name=name;
- }
- public int getId(){
-   return id;
- }
- public void setName(String name){
-   this.name = name;
- }
- public String getName(){
-   return name;
- }
- public String getString(){
-   return String.format("%s [id : %d]" , name , id , counter);
- }
+
+  private static int counter = 0; // счётчик ОБЩИЙ для всех объектов класса
+  private final int id;
+  private String name;
+
+  public Student(String name) {
+    id = counter++; // записываем значение "до увеличения" в качестве id
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  // сеттера для поля counter нет - оно служебное, его нельзя (согласно нашей идее) менять руками
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getString() {
+    // counter в каждый момент времени у всех студентов одинаковый
+    return String.format("%s [id: %d/%d]", name, id, counter);
+  }
 }
+
--- lesson_34/src/Task2.java
+++ lesson_34/src/Task2.java
@@ -1,19 +1,40 @@
 public class Task2 {
-  // access modifiers
-  // private
-  // default
-  // protected
-  // public
 
-  // static
+  // модификаторы доступа (access modifiers) бывают и у атрибутов, и у методов:
+  // - private - доступны только внутри класса
+  // - (default) (по умолчанию) (когда ничего не написано) - доступны всем в рамках пакета (папки)
+  //   (package-private)
+  // - protected - доступны всем потомкам (даже в других пакетах) (потомки будут позже)
+  // - public - доступны всем, можно использовать откуда угодно
+
+  // static - статичный - общий для всех членов класса
+  // статичные атрибуты и методы доступны не через "объект.членКласса", а через "Класс.членКласса"
+  // статичные поля (атрибуты):
+  // - константы
+  // - счётчики
+  // статические методы:
+  // - чтение из строки (парсинг)
+  // - действие, по смыслу неразрывно связанное с конкретым классом, но никак не связанное
+  //   (может быть, пока) с конкретным объектом
+  // - расчёты "в целом"
+  // - проверка условия
+  // - примеры:
+  //   - String.format()
+  //   - Integer.parseInt()
+  // !!!
+  // Статические методы НИЧЕГО не знают про конкретный объект - в статических методах нельзя
+  // использовать this - нет никакого "этого" объекта, есть только сам класс.
 
   public static void main(String[] args) {
-    Student first = new Student("first");
+    Student first = new Student("First");
+    System.out.println(first.getString()); // здесь студентов ровно 1 и Student.counter == 1
     Student second = new Student("Second");
-    Student fourth = new Student("fourth");
-    System.out.println(first.getString());
-    System.out.println(second.getString());
-    System.out.println(fourth.getString());
-
+    System.out.println(first.getString()); // здесь студентов ровно 2 и Student.counter == 2
+    System.out.println(second.getString()); // здесь студентов ровно 2 и Student.counter == 2
+    Student fourth = new Student("Fourth");
+    System.out.println(first.getString()); // здесь студентов ровно 3 и Student.counter == 3
+    System.out.println(second.getString()); // здесь студентов ровно 3 и Student.counter == 3
+    System.out.println(fourth.getString()); // здесь студентов ровно 3 и Student.counter == 3
   }
 }
+

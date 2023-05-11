86dca738e1cf5fe0bd9ef7fb61b43d096d85c9b3
MrKOT92 <mrkot9292@gmail.com>
2023-05-11 09:30:41 +0200

    hm 39

--- hm39/src/Employee.java
+++ hm39/src/Employee.java
@@ -1,11 +1,16 @@
 abstract class Employee {
 
-  protected double baseSalary;
-  protected int experience;
+  public double baseSalary;
+  public int experience;
 
   public Employee(double baseSalary, int experience) {
     this.baseSalary = baseSalary;
     this.experience = experience;
   }
   public abstract double calculateSalary();
+  public double calculateSalary(double additionalBonus) {
+    double increasePercentage = (experience * 0.05) + (additionalBonus / baseSalary);
+    return baseSalary + (baseSalary * increasePercentage);
+  }
 }
+
--- hm39/src/Main.java
+++ hm39/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
-
   public static void main(String[] args) {
-    ITengineer itEngineer = new ITengineer(6500, 3);
-    System.out.println("IT Engineer Salary: " + itEngineer.calculateSalary());
+    ITengineer itEngineer = new ITengineer(6000, 3);
+    double additionalBonus = 1000;
+    System.out.println("IT Engineer Salary: " + itEngineer.calculateSalary(additionalBonus));
 
-    QAengineer qaEngineer = new QAengineer(5500, 5);
+    QAengineer qaEngineer = new QAengineer(5000, 5);
     System.out.println("QA Engineer Salary: " + qaEngineer.calculateSalary());
+
   }
 }
